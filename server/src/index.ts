fa50220db9a32d447747f1ac72ef69715f0ddbab
aspalvis <spalvej15@gmail.com>
2023-02-19 23:03:18 +0200

    textfield added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { Connection } from "./MongoDB/Database";
+import { MongoClient, MongoClientOptions } from "mongodb";
 
 dotenv.config();
 
@@ -13,8 +15,29 @@ app.use(
 );
 const port = 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/", async (req: Request, res: Response) => {
+  // const connection = new Connection("spalvis_store");
+  // await connection.connect();
+  // res.send("connected to db");
+  const uri = process.env.MONGODB_URI;
+  // const client = new MongoClient("mongodb://localhost:27017/");
+  const client = new MongoClient(
+    "mongodb+srv://andris:andris1033@aspalvis.zhmzont.mongodb.net/?retryWrites=true&w=majority",
+    { useUnifiedTopology: true } as MongoClientOptions
+  );
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db("spalvis_store");
+    // await db.collection("tests").insertOne({
+    //   test: "aaaa",
+    // });
+    const result = await db.collections();
+    res.send(result);
+    // Perform database operations here
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(port, () => {
